e4156271d3134bbd5b88b5d814f3b4dce7687faf
Will Nelson <will@pleb.xyz>
2019-06-28 11:23:40 -0700

    limit start, stop to creator

--- bot/src/commands/start.js
+++ bot/src/commands/start.js
@@ -3,6 +3,7 @@ const Game = require('../structures/Game');
 module.exports = async ctx => {
 	const ongoing = await ctx.client.collections.games.findOne({ channel_id: ctx.msg.channel_id });
 	if (!ongoing) return ctx.reply('You must create a game first!');
+	if (ongoing.creator !== ctx.msg.author.id) return ctx.reply(`Only the game creator can start early! Get <@${ongoing.creator}> to run this command.`);
 
 	const game = await new Game(ctx.client, ongoing);
 	return game.start();
--- bot/src/commands/stop.js
+++ bot/src/commands/stop.js
@@ -2,9 +2,8 @@ const Game = require('../structures/Game');
 
 module.exports = async ctx => {
 	const data = await ctx.client.collections.games.findOne({ channel_id: ctx.msg.channel_id });
-	if (!data) {
-		return ctx.reply('There is no game running in this channel.');
-	}
+	if (!data) return ctx.reply('There is no game running in this channel.');
+	if (data.creator !== ctx.msg.author.id) return ctx.reply(`Nice try, but only <@${data.creator}> can end this game.`);
 
 	const game = new Game(ctx.client, data);
 	await game.stop();
